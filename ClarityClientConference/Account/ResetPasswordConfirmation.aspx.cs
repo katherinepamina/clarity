c1faf12688739d7c115b662ff3d39b3983996e86
katherinepamina <katherinelin2016@u.northwestern.edu>
2016-01-24 18:51:17 -0600

    First commit

---
+++ ClarityClientConference/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ClarityClientConference.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ClarityClientConference/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClarityClientConference.Global" Language="C#" %>
