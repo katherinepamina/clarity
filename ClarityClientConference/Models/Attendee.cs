56af7a5b22f45ffc1bb9c50bbe2a84ea5646be3f
katherinepamina <katherinelin2016@u.northwestern.edu>
2016-01-24 19:12:56 -0600

    add client and attendee class

---
+++ ClarityClientConference/Models/Attendee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClarityClientConference.Models
+{
+    public class Attendee
+    {
+        [ScaffoldColumn(false)]
+        public int AttendeeID { get; set; }
+
+        [Required]
+        public int ClientID { get; set; }
+
+        [Required, Display(Name = "Name")]
+        public string AttendeeName { get; set; }
+
+        [Required, Display(Name = "Email")]
+        public string Email { get; set; }
+
+    }
+}
---
+++ ClarityClientConference/Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClarityClientConference.Models
+{
+    public class Client
+    {
+        [ScaffoldColumn(false)]
+        public int ClientID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required, Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required, Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required, Display(Name = "Street")]
+        public string Street { get; set; }
+
+        [Required, Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required, Display(Name = "State")]
+        public string State { get; set; }
+
+        [Required, Display(Name = "ZipCode")]
+        public int ZipCode { get; set; }
+
+    }
+}
